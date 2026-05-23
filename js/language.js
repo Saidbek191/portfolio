fa2018c3fb1b234ba9ef39f17d18b893e97bd77f
Saidbek Kodirov <qodirovsk@gmail.com>
2026-05-23 22:21:35 +0300

    feat: add language switcher and improve SCSS architecture

---
+++ js/language.js
@@ -0,0 +1,44 @@
+const currentBtn = document.querySelector('.language__current');
+const langList = document.querySelector('.language__list');
+const resumeBtn = document.querySelector('.header__btn-resume');
+
+resumeBtn.addEventListener('click', () => {
+	resumeBtn.classList.toggle('active');
+});
+
+currentBtn.addEventListener('click', () => {
+	langList.classList.toggle('active');
+
+	currentBtn.classList.toggle('active');
+});
+
+const langItems = document.querySelectorAll('[data-lang]');
+	langItems.forEach(item => {
+		item.addEventListener('click', () => {
+			const selectedLang = item.dataset.lang;
+
+			currentBtn.textContent = selectedLang.toUpperCase();
+
+			localStorage.setItem('language', selectedLang);
+
+			langList.classList.remove('active');
+
+			currentBtn.classList.remove('active');
+		});
+	});
+	
+	const savedLang = localStorage.getItem('language');
+
+	if(savedLang) {
+		currentBtn.textContent = savedLang.toUpperCase();
+	}
+
+	document.addEventListener('click', (e) => {
+		if(!e.target.closest('.language')) {
+			langList.classList.remove('active');
+
+			currentBtn.classList.remove('active');
+			
+			resumeBtn.classList.remove('active');
+		}
+	});
